a534d8659511dc2b2561cc0d09d1067a69f0caa5
wrumble <rumblewayne@gmail.com>
2016-12-06 18:19:48 +0000

    adds menu to delete lines

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Wayne Rumble on 06/12/2016.
+//  Copyright © 2016 Wayne Rumble. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
